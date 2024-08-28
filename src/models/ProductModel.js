3cab0e48b63cadf5304882f7a30aa5e2fcfe5d27
Mobinul Islam Mahi <mobinulislammahi@gmail.com>
2024-08-28 12:25:30 +0600

    product

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
 
 const DataSchema = mongoose.Schema({
-    
+    productName: { type: String, required: true },
+    productImage: { type: String, required: true },
+    productDesc: { type: String, required: true },
+
 
 }, {timestamps: true,versionKey:false})
 
-const ProductModel = mongoose.model('otps', DataSchema)
+const ProductModel = mongoose.model('products', DataSchema)
 module.exports = ProductModel
