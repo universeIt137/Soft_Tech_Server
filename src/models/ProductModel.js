f9e4196befec74cf0699466f538f46a693331dce
ishanrana363 <ishanrana363@gmail.com>
2024-09-18 12:20:13 +0600

    product model updated

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -1,12 +1,26 @@
 const mongoose = require('mongoose')
 
-const DataSchema = mongoose.Schema({
-    productName: { type: String, required: true },
-    productImage: { type: String, required: true },
-    productDesc: { type: String, required: true },
+const {Schema,model} = mongoose;
 
+const extraSchema = new Schema({
+    extra_description : { type: String, required: true },
+    description_img : { type: String, required: true },
+    description_title : { type: String, required: true },
+},{_id : false});
+
+const productSchema = new Schema({
+    nav_logo: { type: String, required: true },
+    nav_title: { type: String, required: true },
+    nav_description: { type: String, required: true },
+    main_title : { type : String, required : true },
+    live_link : {type : String, required : true  },
+    short_description : {type : String, required : true  },
+    proposal_link : {type : String, required : true  },
+    feature : {type : String, required : true  },
+    feature_logo : {type : String, required : true  },
+    extra_data : [extraSchema],
 
 }, {timestamps: true,versionKey:false})
 
-const ProductModel = mongoose.model('products', DataSchema)
+const ProductModel = model('products', productSchema);
 module.exports = ProductModel
