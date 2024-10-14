d553c7abb5864055d5a79b7cc840f9c46a4f9484
ashikur <ashikur1602@gmail.com>
2024-10-14 16:43:35 +0600

    team member image added

--- src/models/TeamModel.js
+++ src/models/TeamModel.js
@@ -6,6 +6,8 @@ const DataSchema = new mongoose.Schema({
     contact: { type: String, required: true },
     email: { type: String, required: true },
     experience: { type: String, required: true },
+    image: { type: String, required: true },
+
 },
     {
         timestamps: true, versionKey: false
