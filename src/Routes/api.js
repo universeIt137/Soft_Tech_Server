979bb2660847d3babf8a6263c7afb2ca106cc1aa
ishanrana363 <ishanrana363@gmail.com>
2024-12-26 15:01:32 +0600

    product video crud operation completed

--- src/Routes/api.js
+++ src/Routes/api.js
@@ -25,6 +25,8 @@ const { isLogReg } = require('../middleware/registerMiddleware.js')
 const repBankInfoController = require("../controller/representative/repAccountInfoController");
 // client controller
 const clientController = require("../controller/client/clientController");
+// product video upload controller
+const productVideoUploadController = require("../controller/product/productVideoController.js");
 
 
 // Admin Api
@@ -158,6 +160,13 @@ router.delete("/client-delete-admin/:id", isLogin, isAdmin, clientController.cli
 router.put("/client-update-admin/:id", isLogin, isAdmin, clientController.clientUpdateByAdmin);
 router.get("/allClientByRepresentative", isLogInRep, clientController.allClientByRepresentative);
 
+// upload video related api
+
+router.post("/product-video-upload", isLogin, isAdmin, productVideoUploadController.productUploadVideo);
+router.get("/product-all-videos", isLogin, isAdmin, productVideoUploadController.allProductVideos);
+router.get("/product-single-video/:id", isLogin, isAdmin, productVideoUploadController.singleProductVideo);
+router.put("/product-video-update/:id", isLogin, isAdmin, productVideoUploadController.updateProductVideo);
+router.delete("/product-video-delete/:id", isLogin, isAdmin, productVideoUploadController.deleteProductVideo);
 
 
 
